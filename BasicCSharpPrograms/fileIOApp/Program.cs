9ff992d6d8ae325a2a08b381ae4bd0d1304b7d27
Deftness <115406842+deftness02@users.noreply.github.com>
2024-01-07 00:20:40 -0700

    Added basic File I/O app

---
+++ BasicCSharpPrograms/fileIOApp/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Please enter a number greater than 0.");
+
+// Check the player's response and parse.
+int number;
+var isNumber = false;
+Console.ReadLine();
+
+// Check to make sure a number is provided
+while (!isNumber)
+{
+    try
+    {
+        if (Console.ReadLine() != null) number = int.Parse(Console.ReadLine());
+        isNumber = true;
+    }
+    catch
+    {
+        Console.WriteLine("Please enter a number greater than 0.");
+    }
+}
+
+// Log the value entered
+using StreamWriter file = new StreamWriter("logFile.txt", true);
+file.WriteLine(Console.ReadLine());
+
+// Repeat back the contents logged
+File.WriteAllText("logFile.txt", Console.ReadLine());
